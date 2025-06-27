6c95575316934717bd614d38e8c37804d28b0a99
master-manas <manasmadhukar30@gmail.com>
2025-06-27 19:51:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
+function App() {
+
+
+  return (
+   <>
+   <Navbar/>
+   <Hero/>
+    <Highlights/>
+    </>
+  )
+}
+
+export default App
